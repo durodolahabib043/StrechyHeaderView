41046a189285b59f824223a71a0841f79ad811d1
habib <durodolahabib043@gmail.com>
2020-03-15 20:16:23 -0400

    added custom view

---
+++ StrechyHeaderView/INPCollectionViewCell.swift
@@ -0,0 +1,13 @@
+
+import UIKit
+
+class INPCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var singleLabelCell: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- StrechyHeaderView/ViewController.swift
+++ StrechyHeaderView/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UICollectionViewDelegate, UICollectionViewDataSource{
 
+
+
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UINib.init(nibName: "CollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "cellReuseId")
+
     }
 
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+          return 10
+      }
+
+      func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+          let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellReuseId", for: indexPath) as! INPCollectionViewCell
+       //   cell.configure(with: data[indexPath.row])
+
+       // cell.singleRowLabel.text = "cell \(indexPath.row)"
+
+        cell.singleLabelCell.text = "cell \(indexPath.row)"
+          return cell
+      }
 
 }
 
